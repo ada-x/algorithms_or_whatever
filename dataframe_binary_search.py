6adecc258d8da2cdb56b85e69c23e21f2728e23e
xoxi ixchel <xoxi.ixchel@gmail.com>
2016-06-22 15:59:35 -0700

    Add files via upload

---
+++ dataframe_binary_search.py
@@ -0,0 +1,59 @@
+'''
+basic binary search, dataframe implementation. finds a feature from a specified column in a dataframe. formatted to search by "name", however could easily be adapted other types of data. takes 4 inputs: the <name> or search term, the dataframe <df>, the column of the search term <search_col>, and the column containing the feature being searched <feat_col>. dataframe should already be read in so it can be referenced. i've tried to keep the comments as brief yet explanatory as possible. [:
+'''
+
+import math
+
+
+def format_name(name):
+    return name.split(" ")[1] + ", " + name.split(" ")[0]
+
+
+def find_feature(name, df, search_col, feat_col):
+
+    # The length of the dataset
+    length = len(df)
+
+    # can totes omit this if not needed
+    name = format_name(name)
+
+    # starting upper bound
+    upper_bound = length - 1
+
+    # starting lower bound
+    lower_bound = 0
+
+    # index of first split
+    index = math.floor((upper_bound + lower_bound) / 2)
+
+    # first guess at index
+    # can omit formatting if not needed
+    guess = format_name(df[index][search_col])
+
+    # run until the name is equal to the guess
+    # or upper search limit is less than lower search limit
+    while name != guess and upper_bound > lower_bound:
+
+        # If name comes before the guess
+        if name < guess:
+
+            # upper search bound gets lowered before slicing again
+            upper_bound = index - 1
+
+        else:
+            # if the name comes after guess
+            # raise the lower bound
+            lower_bound = index + 1
+
+        # slice again, new index based on new bounds
+        index = math.floor((upper_bound + lower_bound) / 2)
+
+        # get next guess. formatting can be omitted
+        guess = format_name(df[index][search_col])
+
+    if name == guess:
+        return df[index][feature_col]
+    else:
+        # didn't find the feature, return -1
+        return -1
+
